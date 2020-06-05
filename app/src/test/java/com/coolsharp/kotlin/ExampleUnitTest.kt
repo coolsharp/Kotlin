d77e3e6651c273f84b37c09e81698cb058048835
coolsharp <coolsharp@wemakeprice.com>
2020-06-05 18:34:07 +0900

    DFS 작성

--- app/src/test/java/com/coolsharp/kotlin/ExampleUnitTest.kt
+++ app/src/test/java/com/coolsharp/kotlin/ExampleUnitTest.kt
@@ -14,4 +14,92 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    private val MAX_SIZE = 7
+
+    val dx = arrayOf(-1, 0, 1,  0)
+    val dy = arrayOf( 0, 1, 0, -1)
+
+    var n = 7 // 행과 열의 수
+    var group_id = 0 // 단지의 번호로 첫번째 단지부터 1로 시작
+
+    val groups = IntArray(MAX_SIZE * MAX_SIZE) // 각 단지별 집의 수
+
+    val map = arrayOf(
+        intArrayOf(0,1,1,0,1,0,0),
+        intArrayOf(0,1,1,0,1,0,1),
+        intArrayOf(1,1,1,0,1,0,1),
+        intArrayOf(0,0,0,0,1,1,1),
+        intArrayOf(0,1,0,0,0,0,0),
+        intArrayOf(0,1,1,1,1,1,0),
+        intArrayOf(0,1,1,1,0,0,0))
+
+    var visited = arrayOf<Array<Boolean>>()
+
+    @Test
+    fun testDfs() {
+        for (i in 0..MAX_SIZE) {
+            var array = arrayOf<Boolean>()
+            for (j in 0..MAX_SIZE) {
+                array += false
+            }
+            visited += array
+        }
+        // 전체 지도 탐색
+//        = > = = > = =
+//        = = = = = = =
+//        = = = = = = =
+//        = = = = = = =
+//        = > = = = = =
+//        = = = = = = =
+//        = = = = = = =
+        for (i in 0 until n) {
+            for (j in 0 until n) {
+                // 집이면서 방문하지 않았다면 -> 방문
+                if (map[i][j] == 1 && !visited[i][j]) {
+
+                    // 해당 지역에 단지 id를 부여하고
+                    group_id++
+
+                    print(">[%s][%s]".format(i, j))
+
+                    // 탐색
+                    dfs_recursion(i, j)
+                    //dfs_stack(i, j);
+                    //bfs(i, j);
+                }
+                else {
+                    print("=[%s][%s]".format(i, j))
+                }
+                println()
+            }
+        }
+
+        println("그룹 ID " + group_id)
+        for (i in 1..group_id) {
+            println(groups[i])
+        }
+    }
+
+    fun dfs_recursion(x: Int, y: Int) {
+
+        // 함수에 들어왔으면 -> 방문으로 표시
+        visited[x][y] = true
+        // 해당 단지의 집의 수를 증가시킴
+        groups[group_id]++
+
+        // 해당 위치의 주변을 확인
+        for (i in 0..3) {
+            val nx: Int = x + dx[i]
+            val ny: Int = y + dy[i]
+
+            // 지도를 벗어나지 않고
+            if (nx in 0 until n && 0 <= ny && ny < n) {
+                // 집이면서 방문하지 않았다면 -> 방문
+                if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                    dfs_recursion(nx, ny)
+                }
+            }
+        }
+    }
 }
